054afa9689cae1477200bc1f0ca2b285ee29dc37
dlspm <q0978298512@gmail.com>
2017-11-24 08:55:47 +0800

    update last push

--- src/week_/MessageBar.java
+++ src/week_/MessageBar.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package easypainter;
+package week_;
 
 import java.awt.*;
 import java.awt.event.*;
